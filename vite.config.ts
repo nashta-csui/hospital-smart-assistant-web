d91b59de28fc1f523cf5962c08cdb27b011f02f7
Vito Secona <secona00@gmail.com>
2026-02-11 17:11:07 +0700

    init deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // currently deploying to github actions, so we need to set this base path
+  base: '/hospital-smart-assistant-web/',
   plugins: [
     react({
       babel: {
