2482d88d8db659b34ba0b2bd522770453df08ad7
Vito Secona <secona00@gmail.com>
2026-03-01 14:07:40 +0700

    chore: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +13,11 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
