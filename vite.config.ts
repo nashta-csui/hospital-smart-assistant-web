90dc25f1dd684c484ac75e57e5004f3feb2ce424
Vito Secona <secona00@gmail.com>
2026-03-02 20:23:12 +0700

    feat: add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './coverage',
+    },
   },
 })
